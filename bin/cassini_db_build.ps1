8530da499b84b050986896907bfa92eede2b1743
jeffdalton <jeff@simpleway.io>
2018-11-22 21:15:12 -0500

    clean up of powershell script

--- bin/cassini_db_build.ps1
+++ bin/cassini_db_build.ps1
@@ -34,27 +34,32 @@ function Build-Normalize-Script($tables_list, $drop_template, $lookup_template, 
 $config=Get-Content -Path config/cassini_db_build.json -Raw | ConvertFrom-Json
 
 # Prepare build settings
-$db = $config.db
-$base_dir = (Get-Location).ToString() + "/"
-$build_dir = $base_dir + $config.build_dir
-$drop_template = $base_dir + $config.template_dir + $config.drop_template
-$lookup_template = $base_dir + $config.template_dir + $config.lookup_template
-$import_template = $base_dir + $config.template_dir + $config.import_template
-$normalize_template = $base_dir + $config.template_dir + $config.normalize_template
-$import_file = $base_dir + $config.import_file
-$build_file = $build_dir + $config.build_file
-Write-Host "Target Database ${db}"
+$db = $config.db; Write-Host "db $db"
+$base_dir = (Get-Location).ToString() + "/"; Write-Host "base_dir $base_dir"
+$build_dir = $base_dir + $config.build_dir; Write-Host "build_dir $build_dir"
+$drop_template = $base_dir + $config.template_dir + $config.drop_template; Write-Host "drop_template $drop_template" 
+$lookup_template = $base_dir + $config.template_dir + $config.lookup_template; Write-Host "lookup_template $lookup_template"
+$import_template = $base_dir + $config.template_dir + $config.import_template; Write-Host "import_template $import_template"
+$normalize_template = $base_dir + $config.template_dir + $config.normalize_template; Write-Host "normalize_template $normalize_template"
+$import_file = $base_dir + $config.import_file; Write-Host "import_file $import_file"
+$build_file = $build_dir + $config.build_file; Write-Host "build_file $build_file"
 
 if (Test-Path -Path $build_dir)
 {
-	Remove-Item $build_dir -Force -Recurse 
+	Remove-Item $build_dir -Force -Recurse | Out-Null
 }
-New-Item -ItemType Directory -Force -Path $build_dir
+New-Item -ItemType Directory -Force -Path $build_dir | Out-Null
+if (!$?) {
+	Write-Error "Error End $($MyInvocation.MyCommand.Name) @ $(Get-Date)"
+	exit
+}
+
 Build-Import-Script $import_template $import_file $build_file
 Build-Normalize-Script $config.normalize_tables $drop_template $lookup_template $normalize_template $build_file
-psql $db -f $build_file
+psql -V ON_ERROR_STOP=1
+psql $db -f $build_file 
 if (!$?) {
-	Write-Output "Error End $($MyInvocation.MyCommand.Name) @ $(Get-Date)"
+	Write-Error "Error End $($MyInvocation.MyCommand.Name) @ $(Get-Date)"
 	exit
 }
 
