76968e78ea2a0957af209eec3c56980f2b95f425
jeffdalton <jeff@simpleway.io>
2018-12-07 21:44:13 -0500

    Finished BASH version of cassini load script

--- bin/cassini_db_build.sh
+++ bin/cassini_db_build.sh
@@ -3,18 +3,14 @@ echo "Start $0 @ $(date)"
 
 json_config="config/cassini_db_build.json"
 
-function build_import_script () {
-    import_template = $1; echo "import_template $import_template"
-    import_file = $2; echo "import_file $import_file"
-    build_file = $3; echo "build_file $build_file"
-}
-
 function build_normalize_script () {
     $tables_list = $1; echo "tables_list $tables_list"
     $drop_template = $2; echo "drop_template $drop_template"
     $lookup_template = $3; echo "lookup_template $lookup_template"
     $normalize_template = $4; echo "normalize_template $normalize_template"
     $build_file = $5; echo "build_file $build_file"
+
+
 }
 
 function get_json_value() {
@@ -33,6 +29,8 @@ normalize_template=$(get_json_value '.normalize_template');normalize_template="$
 import_file=$(get_json_value '.import_file');import_file="${base_dir}${import_file}"; echo "import_file ${import_file}"
 build_file=$(get_json_value '.build_file');build_file="${build_dir}${build_file}"; echo "build_file ${build_file}"
 
+
+# Clean Build Directory
 if [ -d $build_dir ]; then
     rm -rf $build_dir
 fi
@@ -42,12 +40,33 @@ if [ ! $? -eq 0 ]; then
     exit 1
 fi
 
+# Build Import Script
 cat $import_template | sed 's,\[IMPORT_FILE\],'"${import_file}"',' >> $build_file
 
+# Build Normalize Script
 cat $json_config | jq -c '.normalize_tables | .[]' | while IFS='' read line;do
     table=$(echo "$line" | jq --raw-output '.table')
     column=$(echo "$line" | jq --raw-output '.column')
     isLookup=$(echo "$line" | jq --raw-output '.isLookup')
     echo "table $table  column $column  isLookup $isLookup"
+
+    cat $drop_template | sed 's,\[TABLE\],'"${table}"',' >> $build_file
+
+    if [ $isLookup == "true" ]; then
+        cat $lookup_template | 
+            sed 's,\[TABLE\],'"${table}"',' |
+            sed 's,\[COLUMN\],'"${column}"',' >> $build_file
+    fi
 done
 
+cat $normalize_template >> $build_file
+
+psql -V ON_ERROR_STOP=1
+
+psql $db -f $build_file
+if [ ! $? -eq 0 ]; then
+    echo "Error End $0 @ $(date)"
+    exit
+fi
+
+echo "Normal End $0 @ $(date)"
